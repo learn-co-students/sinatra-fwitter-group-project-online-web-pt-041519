e3da3bcbc42ac2dd5deef8f0da7a737b9000663a
Cody <cody.frank30@gmail.com>
2019-08-08 21:12:16 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,28 @@
 class TweetsController < ApplicationController
 
+    get '/tweets' do
+        if logged_in?
+            @tweets = Tweet.all
+          erb :'tweets/index'
+        else
+          redirect to '/login'
+        end
+    end
+
+    get '/tweets/new' do
+      if logged_in?
+        erb :'tweets/new'
+      else
+        redirect to '/login'
+      end
+    end
+
+    post '/tweets' do
+        tweet = Tweet.new(params[:content])
+        tweet.save
+    end
+
+    
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,49 @@
 class UsersController < ApplicationController
 
     get '/login' do
+      if logged_in?
+        redirect to '/tweets'
+      else
         erb :'users/login'
+      end
     end
 
     post '/login' do
-        @user = User.find_by(username: params[:username], password: params[:password])
-        if @user
-            session[:user_id] = @user.id
-            redirect '/tweets'
-        end 
+      user = User.find_by(:username => params[:username])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect to '/tweets'
+      else
+        redirect to '/signup'
+      end 
     end
 
     get '/signup' do
+      if logged_in?
+        redirect to '/tweets'
+      else
         erb :'users/signup'
+      end
+    end
+
+    post '/signup' do
+      if params[:username] == "" || params[:email] == "" || params[:password] == ""
+        redirect to '/signup'
+      else
+        @user = User.new(username: params[:username], password: params[:password], email: params[:email])
+        @user.save
+        session[:user_id] = @user.id
+        redirect '/tweets'
+      end
+    end
+
+    get '/logout' do
+        if logged_in?
+          session.clear
+          redirect to '/login'
+        else
+          redirect to '/login'
+        end
     end
 
 
