c23e0252205397a528cc3b82e28d00519273de0a
github username <github email address>
2019-08-17 22:47:59 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,24 +3,28 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
-    enable :sessions
     set :public_folder, 'public'
     set :views, 'app/views'
-    set :session_secret, "password_security"
+    enable :sessions
+    set :session_secret, "remember_not_to_use_an_easy_password_like_this"
   end
 
   get '/' do
-    erb :index
+    erb :'users/homepage'
   end
 
   helpers do
+
     def logged_in?
-      !!session[:user_id]
+      !!current_user
     end
 
     def current_user
       @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
+
   end
-  
+
+
+
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,90 +1,80 @@
 class TweetsController < ApplicationController
+
   get '/tweets' do
-    if logged_in?
-      @tweets = Tweet.all
-      erb :'tweets/tweets'
-    else
-      redirect to '/login'
-    end
+      if logged_in?
+          @tweets = Tweet.all
+        erb :'tweets/index'
+      else
+        redirect to '/login'
+      end
   end
 
-  # new tweet form 
   get '/tweets/new' do
     if logged_in?
-      erb :"tweets/create_tweet"
-    else
-      redirect to "/login"
-    end
-  end
-
-  # create + save tweet
-  post '/tweets' do
-    if params[:content].empty?
-      redirect '/tweets/new'
-    else 
-      tweet = current_user.tweets.build(content: params[:content])
-      if tweet.save
-        redirect to "/tweets/#{tweet.id}"
-      else
-        redirect to "/tweets/new"
-      end      
-    end
-  end
-
-  # show a tweet 
-  get '/tweets/:id' do
-    if logged_in?
-      @tweet = Tweet.find(params[:id])
-      erb :'tweets/show_tweet'
-    else
-      redirect to "/login"
-    end
-  end
-
-  # show edit form is user is logged in 
-  get '/tweets/:id/edit' do
-    if logged_in?
-      @tweet = Tweet.find(params[:id])
-      if @tweet.user == current_user
-        erb :'tweets/edit_tweet'
-      else
-        redirect to '/tweets'
-      end
+      erb :'tweets/new'
     else
       redirect to '/login'
     end
   end
 
-  # edit tweet if user is logged in 
-  patch '/tweets/:id' do  
-    if logged_in?   
-      if params[:content].empty?
-        redirect "/tweets/#{params[:id]}/edit"
-      else 
-        # Find tweet and update
-        @tweet = Tweet.find(params[:id])
-        if @tweet.user == current_user
-          @tweet.update(content: params[:content])
-          redirect "/tweets/#{@tweet.id}"
-        else 
-          redirect '/login'
-        end
+  post '/tweets' do
+    if params[:content]==""
+      redirect to '/tweets/new'
+    else
+      @tweet = Tweet.new(content: params[:content])
+      @tweet.save
+      current_user.tweets << @tweet
+      redirect "/tweets/#{@tweet.id}"
+    end
+  end
+
+  get '/tweets/:id' do
+    @tweets = Tweet.all
+    @tweet = Tweet.find_by_id(params[:id])
+    if logged_in?
+      erb :'tweets/show'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if logged_in?
+      if params[:content] == ""
+        redirect to "/tweets/#{params[:id]}/edit"
+      else
+        @tweet.content = params[:content]
+        @tweet.save
+        redirect "/tweets/#{@tweet.id}"
       end
     else
       redirect '/login'
-    end    
+    end
   end
 
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if logged_in?
+      if @tweet.user == current_user
+        erb :'tweets/edit'
+      end
+    else
+      redirect '/login'
+    end
+  end
 
   delete '/tweets/:id/delete' do
-    if logged_in? 
-      @tweet = Tweet.find(params[:id])      
-      if @tweet.user == current_user
-        @tweet.destroy
-      end
+    @tweet = Tweet.find_by_id(params[:id])
+    if logged_in? && @tweet.user == current_user
+      @tweet.delete
       redirect '/tweets'
     else
       redirect '/login'
-    end    
+    end
   end
+
+  
+
+
 end
